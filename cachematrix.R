f6f3784f52f37cc7aff1a2bcae3eca8b564c0110
Matt Bellis <matthew.bellis@novartis.com>
2021-06-07 11:47:05 -0400

    Filled functions templates for hw assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Homework Assignment 3, cache and retreive the inverse of a matrix
 
-## Write a short comment describing this function
-
+## This will create a list which contains methods to get/set the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(mInverse) inverse <<- mInverse
+  getInverse <- function() inverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This method will check if an existing matrix has already computed the inverse and retrieve it.
+## If not, it will calculate the inverse and then store the value in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  if(!is.null(inverse)) {
+    message("Retrieving cached data")
+    return(inverse)
+  }
+  # not in the cache, calculate it ourselves
+  data <- x$get()
+  # solve with only 1 param will take the inverse
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
 }
+
+## Testing
+# cm <- makeCacheMatrix()
+# cm$set(matrix( c(5, 1, 0, 3,-1, 2, 4, 0,-1), nrow=3, byrow=TRUE))
+# cacheSolve(cm)
